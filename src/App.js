4c54e5067b98283bde8be734b61aeb2efeb30dc8
github username <github email address>
2017-11-06 11:25:13 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import NotFound from './components/NotFound'
+import Home from './components/Home'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>  
+          <Route exact path="/" render={Home}/>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+	return(
+		<div>
+			<h1> James Miele </h1>
+			<h3> Full-Stack Web Developer </h3>
+			<img src='https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAIA_wDGAAAAAQAAAAAAAA2sAAAAJGNkZTA4M2Y1LTgyYTktNGEwMC04YjlkLTA3ZDZjMDMxZWQ0Nw.jpg'></img>
+		</div>
+	)
+}
+
+export default Home
